bcd7c8ab9ebd1e27e11ef10fc563b60273cf22d1
STanwar2024 <stanwar@rikoouu.com>
2025-01-14 21:58:25 +0530

    forge up 7.6 + misc deps updated

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const sqlite = require('better-sqlite3');
+const sqlite = require('sqlite3');
 const { APP_CONF_FOLDER } = require('./config/core_config');
 
 const dbPath = path.join(APP_CONF_FOLDER, 'data.db');
